0f44ffe46c09f47371b8528e19c3cce9cdb64e04
leahwhitfield <leah.whitfield@spektrix.com>
2024-12-04 08:24:58 +0000

    Day 4 vertical, horizontal & diagonal

--- AdventOfCode2024.Test/Day4/Day4Test.cs
+++ AdventOfCode2024.Test/Day4/Day4Test.cs
@@ -11,10 +11,91 @@ public class Day4Test
     }
 
     #region Part1
+
+    [Test]
+    public void ShouldSetCharArray()
+    {
+        Assert.That(_day4.Wordsearch[0][0], Is.EqualTo('M'));
+        Assert.That(_day4.Wordsearch[9][8], Is.EqualTo('S'));
+    }
+
+    [Test]
+    public void ShouldFindHorizontalXmas()
+    {
+        var wordsearch = new[] { new[] { 'M', 'X', 'M', 'A', 'S', 'T' } };
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ShouldFindReverseHorizontalXmas()
+    {
+        var wordsearch = new[] { new[] { 'M', 'S', 'A', 'M', 'X', 'T' } };
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ShouldFindVerticalXmas()
+    {
+        char[][] wordsearch = [['X'], ['M'], ['A'], ['S']];
+
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void ShouldFindReverseVerticalXmas()
+    {
+        char[][] wordsearch = [['S'], ['A'], ['M'], ['X']];
+
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ShouldNotFindIncompleteReverseVerticalXmas()
+    {
+        char[][] wordsearch = [['X'], ['A'], ['M'], ['X']];
+
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldFindDiagonalXmas()
+    {
+        char[][] wordsearch =
+        [
+            ['X'],
+            ['Q', 'M'],
+            ['Q', 'Q', 'A'],
+            ['Q', 'Q', 'Q', 'S']
+        ];
+
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void ShouldFindReverseDiagonalXmas()
+    {
+        char[][] wordsearch =
+        [
+            ['S'],
+            ['Q', 'A'],
+            ['Q', 'Q', 'M'],
+            ['Q', 'Q', 'Q', 'X']
+        ];
+
+        var count = _day4.WordCount("XMAS", wordsearch);
+        Assert.That(count, Is.EqualTo(1));
+    }
+
     [Test]
     public void ShouldDoPart1()
     {
-        Assert.That(_day4.Part1(), Is.EqualTo(0));
+        Assert.That(_day4.Part1(), Is.EqualTo(18));
     }
 
     [Test]
@@ -27,6 +108,7 @@ public class Day4Test
     #endregion
 
     #region Part2
+
     [Test]
     public void ShouldDoPart2()
     {
--- AdventOfCode2024/Day4/Day4.cs
+++ AdventOfCode2024/Day4/Day4.cs
@@ -4,14 +4,81 @@ namespace AdventOfCode2024.Day4
 {
     public class Day4(bool useActual = false) : Challenge('4', useActual)
     {
+        public char[][] Wordsearch { get; private set; }
+
         public override void LoadData()
         {
+            Wordsearch = new char[Data.Count][];
+            for (int i = 0; i < Data.Count; i++)
+            {
+                Wordsearch[i] = Data[i].ToCharArray();
+            }
+        }
 
+        public int WordCount(string word, char[][] wordsearch)
+        {
+            int total = 0;
+            for (int i = 0; i < wordsearch.Length; i++)
+            {
+                var line = wordsearch[i];
+                for (var j = 0; j < line.Length; j++)
+                {
+                    var letter = line[j];
+                    if (letter == word[0])
+                    {
+                        if (MatchesWord(wordsearch, i, j, word)) total++;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        private bool MatchesWord(char[][] wordsearch, int lineIndex, int letterIndex, string word)
+        {
+            if (IsHorizontalMatch(wordsearch, lineIndex, letterIndex, word)) return true;
+            if (IsVerticalMatch(wordsearch, lineIndex, letterIndex, word)) return true;
+            if (IsDiagonalMatch(wordsearch, lineIndex, letterIndex, word)) return true;
+
+            return false;
+        }
+
+        private bool IsHorizontalMatch(char[][] wordsearch, int lineIndex, int letterIndex, string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (wordsearch[lineIndex].Length <= letterIndex + i) return false;
+                if (wordsearch[lineIndex][letterIndex + i] != word[i]) return false;
+            }
+
+            return true;
+        }
+        
+        private bool IsVerticalMatch(char[][] wordsearch, int lineIndex, int letterIndex, string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (wordsearch.Length <= lineIndex + i) return false;
+                if (wordsearch[lineIndex + i][letterIndex] != word[i]) return false;
+            }
+
+            return true;
+        }
+        
+        private bool IsDiagonalMatch(char[][] wordsearch, int lineIndex, int letterIndex, string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (wordsearch.Length <= i + lineIndex || wordsearch[i + lineIndex].Length <= i + letterIndex) return false;
+                if (wordsearch[i + lineIndex][i + letterIndex] != word[i]) return false;
+            }
+
+            return true;
         }
 
         public override int Part1()
         {
-            return 0;
+            return WordCount("XMAS", Wordsearch);
         }
 
         public override int Part2()
