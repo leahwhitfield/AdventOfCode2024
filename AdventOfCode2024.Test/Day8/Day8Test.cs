7b22f867f94ecfd1d0c30d359c04792b8c7ca132
leahwhitfield <leah.whitfield@spektrix.com>
2024-12-07 11:32:42 +0000

    Day 8 setup

---
+++ AdventOfCode2024.Test/Day8/Day8Test.cs
@@ -0,0 +1,44 @@
+namespace AdventOfCode2024.Test.Day8;
+
+public class Day8Test
+{
+    private AdventOfCode2024.Day8.Day8 _day8;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _day8 = new();
+    }
+
+    #region Part1
+    [Test]
+    public void ShouldDoPart1()
+    {
+        Assert.That(_day8.Part1(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldDoPart1_Actual()
+    {
+        _day8 = new(true);
+        Assert.That(_day8.Part1(), Is.EqualTo(0));
+    }
+
+    #endregion
+
+    #region Part2
+    [Test]
+    public void ShouldDoPart2()
+    {
+        Assert.That(_day8.Part2(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldDoPart2_actual()
+    {
+        _day8 = new(true);
+        Assert.That(_day8.Part2(), Is.EqualTo(0));
+    }
+
+    #endregion
+}
---
+++ AdventOfCode2024/Day8/Day8.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2024.Helpers;
+
+namespace AdventOfCode2024.Day8
+{
+    public class Day8(bool useActual = false) : Challenge('8', useActual)
+    {
+        public override void LoadData()
+        {
+        }
+
+        public override int Part1()
+        {
+            return 0;
+        }
+
+
+        public override int Part2()
+        {
+            return 0;
+        }
+    }
+}
