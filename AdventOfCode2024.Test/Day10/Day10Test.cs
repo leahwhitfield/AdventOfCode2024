9f4f783d60943cef6318e31743143b8d3fe666d8
leahwhitfield <leah.whitfield@spektrix.com>
2024-12-10 14:13:40 +0000

    Day 10 part 1

--- AdventOfCode2024.Test/Day10/Day10Test.cs
+++ AdventOfCode2024.Test/Day10/Day10Test.cs
@@ -12,16 +12,29 @@ public class Day10Test
 
     #region Part1
     [Test]
+    public void ShouldLoadData()
+    {
+        Assert.That(_day10.Map[1][1], Is.EqualTo(8));
+    }
+        
+    [Test]
+    public void ShouldGetStartingPositions()
+    {
+        Assert.That(_day10.GetStartingPositions()[0], Is.EqualTo((0, 2)));
+        Assert.That(_day10.GetStartingPositions()[2], Is.EqualTo((2, 4)));
+    }
+    
+    [Test]
     public void ShouldDoPart1()
     {
-        Assert.That(_day10.Part1(), Is.EqualTo(0));
+        Assert.That(_day10.Part1(), Is.EqualTo(36));
     }
 
     [Test]
     public void ShouldDoPart1_Actual()
     {
         _day10 = new(true);
-        Assert.That(_day10.Part1(), Is.EqualTo(0));
+        Assert.That(_day10.Part1(), Is.EqualTo(557));
     }
 
     #endregion
--- AdventOfCode2024/Day10/Day10.cs
+++ AdventOfCode2024/Day10/Day10.cs
@@ -4,13 +4,107 @@ namespace AdventOfCode2024.Day10
 {
     public class Day10(bool useActual = false) : Challenge(10, useActual)
     {
+        public int[][] Map;
+
         public override void LoadData()
         {
+            Map = new int[Data.Count][];
+            for (var i = 0; i < Data.Count; i++)
+            {
+                var line = Data[i];
+                Map[i] = line.Select(x => int.Parse(x.ToString())).ToArray();
+            }
+        }
+
+        public int FindTrailheads()
+        {
+            var startingPositions = GetStartingPositions();
+            int totalTrailheadScore = 0;
+
+            foreach (var startingPosition in startingPositions)
+            {
+                List<(int, int)> currentTrailHead = [startingPosition];
+
+                var pointsReached = NavigateTrailhead(currentTrailHead);
+                totalTrailheadScore += pointsReached.Count;
+            }
+
+            return totalTrailheadScore;
+        }
+
+        private HashSet<(int, int)> NavigateTrailhead(List<(int, int)> currentTrailHead)
+        {
+            var currentPosition = currentTrailHead.Last();
+            List<(int, int)> directions = FindPossibleDirections(currentPosition, currentTrailHead);
+            HashSet<(int, int)> nineHeightPositionsReached = [];
+
+
+            foreach (var direction in directions)
+            {
+                currentTrailHead.Add((direction.Item1, direction.Item2));
+
+                nineHeightPositionsReached =
+                    nineHeightPositionsReached.Union(NavigateTrailhead(currentTrailHead)).ToHashSet();
+                currentTrailHead.Remove(currentTrailHead.Last());
+            }
+
+            if (currentTrailHead.Count > 9)
+            {
+                nineHeightPositionsReached.Add(currentPosition);
+            }
+
+            return nineHeightPositionsReached;
+        }
+
+        private List<(int, int)> FindPossibleDirections((int, int) currentPosition, List<(int, int)> currentTrailHead)
+        {
+            List<(int, int)> possibleDirections = [];
+            // Look right
+            if (LookInDirection(currentPosition, currentTrailHead.Count, 0, 1))
+                possibleDirections.Add((currentPosition.Item1, currentPosition.Item2 + 1));
+
+            // Look left
+            if (LookInDirection(currentPosition, currentTrailHead.Count, 0, -1))
+                possibleDirections.Add((currentPosition.Item1, currentPosition.Item2 - 1));
+
+            // Look down
+            if (LookInDirection(currentPosition, currentTrailHead.Count, 1, 0))
+                possibleDirections.Add((currentPosition.Item1 + 1, currentPosition.Item2));
+
+            // Look up
+            if (LookInDirection(currentPosition, currentTrailHead.Count, -1, 0))
+                possibleDirections.Add((currentPosition.Item1 - 1, currentPosition.Item2));
+
+            return possibleDirections;
+        }
+
+        private bool LookInDirection((int, int) currentPosition, int nextNumber, int y, int x)
+        {
+            var positionToCheck = (currentPosition.Item1 + y, currentPosition.Item2 + x);
+            if (positionToCheck.Item1 < 0 || positionToCheck.Item2 < 0 || positionToCheck.Item1 >= Map.Length ||
+                positionToCheck.Item2 >= Map[0].Length) return false;
+            var potentialNextStep = Map[positionToCheck.Item1][positionToCheck.Item2];
+            return potentialNextStep == nextNumber;
+        }
+
+
+        public List<(int, int)> GetStartingPositions()
+        {
+            List<(int, int)> startingPositions = [];
+            for (int i = 0; i < Map.Length; i++)
+            {
+                for (int j = 0; j < Map[i].Length; j++)
+                {
+                    if (Map[i][j] == 0) startingPositions.Add((i, j));
+                }
+            }
+
+            return startingPositions;
         }
 
         public override long Part1()
         {
-            return 0;
+            return FindTrailheads();
         }
 
 
