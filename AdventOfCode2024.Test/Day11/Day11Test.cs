989eeda62b1ce688c270b8346a6804a7feddfafb
leahwhitfield <leah.whitfield@spektrix.com>
2024-12-11 09:29:59 +0000

    Day 11 part 1

--- AdventOfCode2024.Test/Day11/Day11Test.cs
+++ AdventOfCode2024.Test/Day11/Day11Test.cs
@@ -12,16 +12,49 @@ public class Day11Test
 
     #region Part1
     [Test]
+    public void ShouldLoadData()
+    {
+        int[] expected = [125,17];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));
+    }        
+    
+    [Test]
+    public void ShouldMoveStonesOnBlink()
+    {
+        _day11.Blink();
+        _day11.Blink();
+        int[] expected = [253, 0, 2024, 14168];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));
+        
+        _day11.Blink();
+        expected = [512072, 1, 20, 24, 28676032];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));
+        
+        _day11.Blink();
+        expected = [512, 72, 2024, 2, 0, 2, 4, 2867, 6032];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));        
+        
+        _day11.Blink();
+        expected = [1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));        
+        
+        _day11.Blink();
+        expected = [2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2];
+        Assert.That(_day11.Stones, Is.EqualTo(expected));
+        Assert.That(_day11.Stones.Length, Is.EqualTo(22));
+    }    
+    
+    [Test]
     public void ShouldDoPart1()
     {
-        Assert.That(_day11.Part1(), Is.EqualTo(0));
+        Assert.That(_day11.Part1(), Is.EqualTo(55312));
     }
 
     [Test]
     public void ShouldDoPart1_Actual()
     {
         _day11 = new(true);
-        Assert.That(_day11.Part1(), Is.EqualTo(0));
+        Assert.That(_day11.Part1(), Is.EqualTo(194782));
     }
 
     #endregion
--- AdventOfCode2024/Day11/Day11.cs
+++ AdventOfCode2024/Day11/Day11.cs
@@ -4,14 +4,40 @@ namespace AdventOfCode2024.Day11
 {
     public class Day11(bool useActual = false) : Challenge(11, useActual)
     {
+        public long[] Stones = [];
+
         public override void LoadData()
         {
+            Stones = Data[0].Split(" ").Select(long.Parse).ToArray();
+        }
+
+        public void Blink()
+        {
+            List<long> newLayout = [];
+            foreach (var stone in Stones)
+            {
+                if (stone == 0) newLayout.Add(1);
+                else if (stone.ToString().Length % 2 == 0)
+                {
+                    var newStone = stone.ToString();
+                    newLayout.Add(long.Parse(newStone[new Range(0, newStone.Length / 2)]));
+                    newLayout.Add(long.Parse(newStone[new Range(newStone.Length / 2, newStone.Length)]));
+                }
+                else newLayout.Add(stone * 2024);
+            }
+
+            Stones = newLayout.ToArray();
         }
 
 
         public override long Part1()
         {
-            return 0;
+            for (int i = 0; i < 25; i++)
+            {
+                Blink();
+            }
+
+            return Stones.Length;
         }
 
 
